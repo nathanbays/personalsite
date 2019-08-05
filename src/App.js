aefb60cd58cccb744309a236279407b873ce66ec
Matthew Vilhauer <matthewvilhauer@gmail.com>
2019-08-04 20:54:31 -0400

    Removed target=_blank to keep mail in the same tab.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="pageHeader">Nathan Bays</div>
-        <div className="email"><span className="label">E: </span><a className="label" href="mailto:NB@nathanbays.com" target="_blank" rel="noopener noreferrer">NB@nathanbays.com</a></div>
+        <div className="email"><span className="label">E: </span><a className="label" href="mailto:NB@nathanbays.com">NB@nathanbays.com</a></div>
         <div className="phone"><span className="label">P: </span><a className="label" href="tel:4237946101">423.794.6101</a></div>
       </header>
     </div>
