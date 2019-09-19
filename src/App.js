ad23f828b6856b774db6d7a31e639ef6f2922306
Matthew Vilhauer <matthewvilhauer@gmail.com>
2019-09-19 00:31:09 -0400

    Upgraded npm packages to fix security vulnerabilities.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,8 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="pageHeader">Nathan Bays</div>
-        <div className="email"><span className="label">E: </span><a className="label" href="mailto:NB@nathanbays.com">NB@nathanbays.com</a></div>
-        <div className="phone"><span className="label">P: </span><a className="label" href="tel:4237946101">423.794.6101</a></div>
+        <div className="email">
+          <span className="label">E: </span>
+          <a className="label" title="Email NB@nathanbays.com" href="mailto:NB@nathanbays.com">NB@nathanbays.com</a>
+        </div>
+        <div className="phone">
+          <span className="label">P: </span>
+          <a className="label" href="tel:4237946101">423.794.6101</a>
+        </div>
       </header>
     </div>
   );
